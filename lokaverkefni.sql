7c65d33169a595b7821e59a6c11b77fdcca13efd
SigurdurAxel0 <sigurdurar263@nemi.tskoli.is>
2017-05-05 15:55:16 +0000

    byrjaður á að setja upplýsingar í tables

--- lokaverkefni.sql
+++ lokaverkefni.sql
@@ -2,33 +2,33 @@ create database 1007002630_lokaverk;
 use database 1007002630_lokaverk;
 
 
-CREATE TABLE flokkur(
+create table flokkur(
     ID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     nafn VARCHAR(255)
 );
 
-CREATE TABLE utgefandi(
+create table utgefandi(
     ID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     nafn VARCHAR(255)
 );
 
-CREATE TABLE tegund(
+create table tegund(
     ID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     nafn VARCHAR(255)
 );
 
     
-CREATE TABLE flytjandi(
+create table flytjandi(
     ID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     nafn VARCHAR(255),
     faedingardagur INT,
-    lysing VARCHAR(555),
+    lysing VARCHAR(500),
     danardagur INT,
     flokkur_flytjanda INT,
     FOREIGN KEY (flokkur_flytjanda) REFERENCES flokkur(ID)
 );
 
-CREATE TABLE diskur(
+create table diskur(
     ID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     nafn VARCHAR(255),
     utgafudagur INT,
@@ -37,7 +37,7 @@ CREATE TABLE diskur(
 );
 
 
-CREATE TABLE lag(
+create table lag(
     ID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     nafn VARCHAR(255),
     lengd_sek INT,
@@ -49,3 +49,60 @@ CREATE TABLE lag(
     FOREIGN KEY (flytjandi) REFERENCES flytjandi(ID),
     FOREIGN KEY (tegund) REFERENCES tegund(ID)
 );
+
+
+insert into
+    flokkur(nafn)
+values
+	("Hljómsveit"),
+    ("Sóló"),
+    ("Dúó");
+
+
+insert into
+    utgefandi(nafn)
+values
+    ("Robert Stigwood"),
+    ("Nicholas Caldwell"),
+    ("Pat Harrington Jr");
+
+insert into
+    tegund(nafn)
+values
+    ("Troy Shondell"),
+    ("Kitty Kallen"),
+    ("Angus Scrimm"),
+    ("David Bowie"),
+    ("David Margulies");
+
+
+insert into
+    flytjandi(nafn,faedingardagur,lysing,danardagur,flokkur_flytjanda)
+values
+    ("Brian Bedford",1990,"leikari dó 80",1970,3),
+    ("Alan Rickman",1670,"leikari dó 69",NULL,2),
+	("Queen",1970,"Queen var bresk rokkhljómsveit sem kom fram á sjónarsviðið 1970. Hún var stofnuð af Freddie Mercury söngvara sveitarinnar, Roger Taylor trommuleikara og Brian May gítarleikara í London sama ár.",NULL,2),
+    ("René Angélil",1892,"Sóngvari og dó 73",2016,1),
+    ("Dan Haggerty",1921,"leikari dó 74",NULL,1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
